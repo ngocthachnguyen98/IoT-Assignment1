98f4f8d15ddb4c6af5a575c719593d094ab60a67
Tyler <ngocthachnguyen98@gmail.com>
2020-03-27 19:11:07 +1100

    Added exception handling for monitorAndDisplay.py

--- Assignment1/monitorAndDisplay.py
+++ Assignment1/monitorAndDisplay.py
@@ -10,32 +10,39 @@ green = (0, 255, 0)
 blue = (0, 0, 255)
 
 
-# Read and declare variables from config.json file
-with open("config.json", "r") as read_file:
-    data = json.load(read_file)
-
-cold_max = data["cold_max"]
-comfortable_min = data["comfortable_min"]
-comfortable_max = data["comfortable_max"]
-hot_min = data["hot_min"]
-
-
 # Get temperature from Sense Hat
 temp = sense.get_temperature()
 temp = round(temp, 1) # Round up to one decimal point
 
 
-# Display message on Sense Hat
-while True:
-    if temp <= cold_max: background = blue
-    elif comfortable_min < temp < comfortable_max: background = green
-    elif temp >= hot_min: background = red
+try:
+    # Read and declare variables from config.json file
+    with open("config.json", "r") as read_file:
+        data = json.load(read_file)
 
-    sense.show_message(
-        "Temperature: " + str(temp) + "*C",
-        scroll_speed = 0.1,
-        back_colour =  background
-    )
+        # Assign values of temperature range settings
+        cold_max = data["cold_max"]
+        comfortable_min = data["comfortable_min"]
+        comfortable_max = data["comfortable_max"]
+        hot_min = data["hot_min"]
 
-    # Delay 10s and refresh the display
-    sleep(10) 
+        # Display message on Sense Hat
+        while True:
+            if temp <= cold_max: background = blue
+
+            elif comfortable_min < temp < comfortable_max: background = green
+            
+            elif temp >= hot_min: background = red
+
+            sense.show_message(
+                "Temperature: " + str(temp) + "*C",
+                scroll_speed = 0.1,
+                back_colour =  background
+            )
+
+            # Delay 10s and refresh the display
+            sleep(10) 
+except FileNotFoundError: # Cacth exception if file is not found
+    print("ERROR: Non-existing config filename")
+except KeyError: # Catch exception if a certain range of setting is not found
+    print("ERROR: Incorrect key for temperature range setting")
